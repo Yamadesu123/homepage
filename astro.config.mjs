1778c9785c9c2dbd1e5dca4654c9014f93d8e6dd
Wan-Dog <posiyama112@gmail.com>
2026-06-11 19:23:20 +0900

    ユーザー名変更に伴うURLの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // ★ここから追加
-  site: 'https://wan-dog.github.io', // あなたのGitHubのベースURL
+  site: 'https://Yamadesu123.github.io', // あなたのGitHubのベースURL
   base: '/homepage',                 // 公開したいURLの末尾（リポジトリ名）
   // ★ここまで追加
 });
