ea21d6cf996b3ccd31a7cb3d8ca8c42ad6f96c30
Wan-Dog <posiyama112@gmail.com>
2026-06-11 19:18:16 +0900

    サイト公開の準備

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ★ここから追加
+  site: 'https://wan-dog.github.io', // あなたのGitHubのベースURL
+  base: '/homepage',                 // 公開したいURLの末尾（リポジトリ名）
+  // ★ここまで追加
+});
+
